226a60306e5d1979058a1b289c57a3b29deacd9e
raunakm90 <raunakm90@gmail.com>
2017-07-03 17:17:11 -0500

    Clean code and create specific classes

--- TwitterApp/twitter_api.py
+++ TwitterApp/twitter_api.py
@@ -18,35 +18,53 @@ langs = {'ar': 'Arabic', 'bg': 'Bulgarian', 'ca': 'Catalan', 'cs': 'Czech', 'da'
          'ru': 'Russian', 'sk': 'Slovak', 'sl': 'Slovenian', 'sr': 'Serbian', 'sv': 'Swedish', 'th': 'Thai', 'tl': 'Filipino', 'tr': 'Turkish', 'uk': 'Ukrainian', 'ur': 'Urdu',
          'vi': 'Vietnamese', 'zh_CN': 'Chinese (simplified)', 'zh_TW': 'Chinese (traditional)'}
 
+class stats():
+    def __init__(self):
+        self.lang = []
+        self.top_lang = []
+        self.top_tweets = []
+
+    def add_lang(self, lang):
+        self.lang.append(lang)
+
+    def add_top_lang(self, top_lang):
+        self.top_lang.append(top_lang)
+
+    def add_top_tweets(self, tweet_html):
+        self.top_tweets.append(tweet_html)
+
+    def get_stats(self):
+        return self.lang, self.top_lang, self.top_tweets
+
 class twitter_stream_listener(StreamListener):
     """A listener that handles tweets received from the stream
     Basic listener that prints received tweets
     """
 
-    def __init__(self, num_tweets=10, retweet_count=10000):
+    def __init__(self, num_tweets, retweet_count, stats, get_tweet_html):
         self.counter = 0
         self.num_tweets = num_tweets # Numbber of tweets
         self.languages = [] #language of the tweet
         self.retweet_count = retweet_count
         self.top_languages = []
+        self.stats = stats
+        self.get_tweet_html = get_tweet_html
 
     # Tells tweepy what to do when a new tweet is available
     def on_data(self, data):
         try:
             json_data = json.loads(data)
             retweet_count = json_data["retweeted_status"]["retweet_count"]
+            self.stats.add_lang(langs[json_data["lang"]])
 
             # Top tweets
             if retweet_count >= self.retweet_count:
-                print(json_data["text"], retweet_count, langs[json_data["lang"]])
-                self.top_languages.append(langs[json_data["lang"]])
+                # print(json_data["text"], retweet_count, langs[json_data["lang"]])
+                self.stats.add_top_tweets(self.get_tweet_html(json_data['id']))
+                self.stats.add_top_lang(langs[json_data["lang"]])
 
-            self.languages.append(langs[json_data["lang"]])
             self.counter += 1
             if self.counter == self.num_tweets:
-                print(json_data)
-                print(self.languages)
-                print(Counter(self.languages))
                 return False # causes the class to exit
             else:
                 return True # continues to look for a new tweet
@@ -58,22 +76,52 @@ class twitter_stream_listener(StreamListener):
     def on_error(self, status_code):
         print(status_code)
 
+class twitterMain():
+    def __init__(self, num_tweets, retweet_count):
+        self.auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        self.auth.set_access_token(access_token, access_token_secret)
+
+        self.twitter_api = tweepy.API(self.auth)
+        self.num_tweets = num_tweets
+        self.retweet_count = retweet_count
+        self.stats = stats()
+
+    def get_streaming_data(self, search_keywords):
+        listener = twitter_stream_listener(self.num_tweets, self.retweet_count, self.stats, self.get_tweet_html)
+        stream = tweepy.Stream(self.auth, listener)
+        stream.filter(track=search_keywords)
+
+        lang, top_lang, top_tweets = self.stats.get_stats()
+
+        print(Counter(lang))
+        print(Counter(top_lang))
+        print(top_tweets)
+
+    def get_trends(self):
+        trends = self.twitter_api.trends_place(1)
+        trend_data = []
+
+        for trend in trends[0]["trends"]:
+            trend_tweets = []
+            trend_tweets.append(trend['name'])
+            tt = tweepy.Cursor(self.twitter_api.search, q = trend['name']).items(5)
+
+            for t in tt:
+                trend_tweets.append(self.get_tweet_html(t.id))
+
+            trend_data.append(tuple(trend_tweets))
+
+    def get_tweet_html(self, id):
+        oembed = self.twitter_api.get_oembed(id=id, hide_media=True, hide_thread=True)
+        tweet_html = oembed['html'].strip("\n")
+
+        return tweet_html
+
+
 if __name__=='__main__':
-    listener = twitter_stream_listener(num_tweets=20)
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
+    num_tweets = 2
+    retweet_count = 100
 
-    stream = tweepy.Stream(auth, listener)
-    stream.filter(track=['wimbledon'])
-
-    # Search for a given query
-    # twitter_api = tweepy.API(auth)
-    # search_results = tweepy.Cursor(twitter_api.search, q="wimbledon").items(20)
-    # for result in search_results:
-    #     print(result.text)
-    #
-    # # Trends
-    # trends = twitter_api.trends_place(1) #Global trends
-    #
-    # for trend in trends[0]["trends"]:
-    #     print(trend['name'])
+    tw = twitterMain(num_tweets, retweet_count)
+    tw.get_streaming_data(search_keywords=['Wimbledon'])
+    tw.get_trends()
